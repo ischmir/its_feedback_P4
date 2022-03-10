0a5ddb1a9806bc6d3a9abef36ef6a13eb03b3ec8
ischmir <timmhinsch@gmail.com>
2022-03-10 13:59:29 +0100

    dnd

--- p4.js
+++ p4.js
@@ -5,28 +5,19 @@ function _(id){
  }
  let droppedIn = false;
  function drag_start(event) {
-     _('app_status').innerHTML = "Dragging the "+event.target.getAttribute('id');
      event.dataTransfer.dropEffect = "move";
      event.dataTransfer.setData("text", event.target.getAttribute('id') );
- }
- function drag_enter(event) {
-     _('app_status').innerHTML = "You are dragging over the "+event.target.getAttribute('id');
- }
- function drag_leave(event) {
-     _('app_status').innerHTML = "You left the "+event.target.getAttribute('id');
  }
  function drag_drop(event) {
      event.preventDefault(); /* Prevent undesirable default behavior while dropping */
      let elem_id = event.dataTransfer.getData("text");
      event.target.appendChild( _(elem_id) );
-     _('app_status').innerHTML = "Dropped "+elem_id+" into the "+event.target.getAttribute('id');
      _(elem_id).removeAttribute("draggable");
      _(elem_id).style.cursor = "default";
      droppedIn = true;
  }
  function drag_end(event) {
      if(droppedIn == false){
-         _('app_status').innerHTML = "You let the "+event.target.getAttribute('id')+" go.";
      }
      droppedIn = false;
  }
