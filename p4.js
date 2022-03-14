8796ae3da6d34bc425e4513e0aaa59745af63bbe
Mtakat01 <96202664+Mtakat01@users.noreply.github.com>
2022-03-14 12:13:02 +0100

    input details

--- p4.js
+++ p4.js
@@ -89,7 +89,7 @@ document.getElementById("Morgenmessage").innerHTML = greeting;
 function validate(){
 var username=document.getElementById("username").value;
 var password=document.getElementById("password").value;
-if (username=="admin"&& password=="user"){
+if (username=="tihi37072@edu.ucl.dk"&& password=="1234"){
   alert("login succesful");
   return false;
 }
