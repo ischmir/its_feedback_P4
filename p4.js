3b3c60fa032fea3b5a9eae09080129535e0bbd60
ischmir <timmhinsch@gmail.com>
2022-03-15 10:18:12 +0100

    styling, fejlretning, knapper, linking

--- p4.js
+++ p4.js
@@ -25,7 +25,6 @@ function _(id){
      for(let i=0; i < _("drop_zone").children.length; i++){
          alert(_("drop_zone").children[i].id+" is in the drop zone");
      }
-     /* Run Ajax request to pass any data to your server */
  }
 
 
